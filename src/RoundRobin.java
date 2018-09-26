afc24a76e21bd256c5002a4999de2af63e28037b
Edj84 <eduardo.silva.006@acad.pucrs.br>
2018-09-26 01:48:39 -0300

    Added:  boolean checkReadyQueue(int priority)  boolean JobsDone(int numJobs)

--- src/RoundRobin.java
+++ src/RoundRobin.java
@@ -6,27 +6,36 @@ public class RoundRobin {
 			private HashMap ready;
 			private ArrayList<Job> blocked;
 			private ArrayList<Job> done;
-			private int timeSlice;
-			
+			private int timeSlice;			
 						
 			public RoundRobin(int timeSlice) {
 				this.timeSlice = timeSlice;
+				
 				ready = new HashMap<Integer,ArrayList<Job>>();
-					
+				
+				//Inserindo no Hash as filas de prioridade de processos prontos para executar
 				for(int i = 1; i < 10; i++)
 					ready.put(i,new ArrayList<Job>());
 				
+				//Processos bloqueados podem ser armazenados em filas simples, por ordem de chegada.
+				//Considerando que o tempo de IO é constante, basta verificar apenas se o primeiro da fila está novamente pronto para executar.
+				//Se ele não estiver, aqueles que chegaram depois dele, logicamente, também não estarão.
 				blocked = new ArrayList<Job>();
+				
+				//Não há necessidade de ordenar os processos prontos. Utilizei um ArrayList por padronização e pela facilidade de iteração.
 				done = new ArrayList<Job>();
 			}
 			
+			//recebe um novo processo e o inclui na fila adequada conforme seu status
 			public void receiveJob(Job job) {
+				
 				ArrayList<Job> aux = null;
 				
 				switch(job.getStatus()) {
 				
 					case READY:
-						aux = (ArrayList<Job>) ready.get(job.getPriority());											
+						int priority = job.getPriority();
+						aux = (ArrayList<Job>) ready.get(priority);											
 						break;
 					
 					case BLOCKED:
@@ -41,17 +50,26 @@ public class RoundRobin {
 				aux.add(job);				
 			}
 			
-			public void selectJob() {
+			//Verifica se a fila com a prioridade i tem processo pronto para executar
+			public boolean checkReadyQueue(int priority) {
 				
-				Job nextJob = null;
-				ArrayList<Job> queue;
+				ArrayList<Job> aux = (ArrayList<Job>) ready.get(priority);
 				
-				for(int i = 1; nextJob==null && i<10; i++) {
-					queue = (ArrayList<Job>) ready.get(i);
-					
-				}
-				
+				return aux.isEmpty();
 			}
+			
+			//Escolhe o próximo processo a ser executado
+			public void selectNextJob() {
+												
+			}
+			
+			//Verifica se ainda há processos a executar
+			public boolean JobsDone(int numJobs) {
+				
+				return done.size() == numJobs;
+			}
+
+			
 			
 			
 			
